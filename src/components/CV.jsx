f0807d61496b93ce4475d23a98320b2876d0ee22
Itzmar Franco <kairifranco@rocketmail.com>
2021-05-25 01:32:11 -0600

    added profile picture and social links

---
+++ src/components/CV.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import photo from '../assets/images/pp.png'
+import '../assets/css/style.css'
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+import {
+    faGithub,
+    faFacebook,
+    faTwitter,
+    faLinkedinIn
+  } from '@fortawesome/free-brands-svg-icons'
+
+import {
+    faEnvelope,
+    faGlobe,
+}  from '@fortawesome/free-solid-svg-icons'
+
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap'
+
+
+function CV() {
+    let globeIcon = <FontAwesomeIcon icon={faGlobe}/>
+    return (
+        <div className='container'>
+            <div className='left-panel'>
+                <div className='content-left'>
+                    <h1 className='h1'>Itzmar Franco</h1>
+                    <img src={photo} alt='Man standing' className='profilePicture'/>
+                    <div className='socials'>
+                        <a className='social-link animate__animated animate__bounce' target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/kairi-franco-47a041198/'>
+                            <FontAwesomeIcon icon={faLinkedinIn} />
+                        </a>
+                        <a className='social-link' target='_blank' rel='noreferrer' href='https://github.com/itzmarfranco'>
+                            <FontAwesomeIcon icon={faGithub} />
+                        </a>
+                        <a className='social-link' target='_blank' rel='noreferrer' href='https://www.facebook.com/itzmarfranco/'>
+                            <FontAwesomeIcon icon={faFacebook} />
+                        </a>
+                        <a className='social-link' target='_blank' rel='noreferrer' href='https://twitter.com/iItzmar'>
+                            <FontAwesomeIcon icon={faTwitter} />
+                        </a>
+                        <a className='social-link' target='_blank' rel='noreferrer' href='mailto:kairifranco@rocketmail.com'>
+                            <FontAwesomeIcon icon={faEnvelope}/>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div className='right-panel'>
+                <div className='top-nav-bar'>
+                    <Navbar bg="" expand='lg'>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className='mr-auto'>
+                            <Nav.Link href="#home">About</Nav.Link>
+                            <Nav.Link href="#link">Resume</Nav.Link>
+                        </Nav>
+                        <Nav className='mr-3'>
+                            <NavDropdown title={globeIcon} id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Spanish</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">English</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                    </Navbar>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CV
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import CV from './components/CV'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CV/>
   </React.StrictMode>,
   document.getElementById('root')
 );
